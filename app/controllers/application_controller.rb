6e54a81378da818998b769e81168165bde17807d
Lani1321 <lani.berry13@gmail.com>
2016-10-04 13:20:29 -0400

    changed session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,27 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "fwitter_secret"
+    set :session_secret, "secret"
   end
 
   get '/' do
     erb :index
+  end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login?error=You have to be logged in to do that"
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
   
 end
