f8a19e3395ccca2980b3c5f407e22262b60d3114
Lani1321 <lani.berry13@gmail.com>
2016-10-03 23:34:55 -0400

    create list_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004032549) do
+ActiveRecord::Schema.define(version: 20161004033041) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "list_items", force: :cascade do |t|
+    t.integer "list_id"
+    t.integer "item_id"
   end
 
   create_table "lists", force: :cascade do |t|
