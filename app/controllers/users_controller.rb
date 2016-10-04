b1d26712e8e7f42601739c3c48e15a8d641c0b45
Lani1321 <lani.berry13@gmail.com>
2016-10-04 10:39:23 -0400

    add get and post routes to Users controller for user to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,22 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+       erb :'/users/login'
+    else
+       redirect to '/lists'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/lists"
+    else
+      redirect to '/signup'
+    end
+  end
+
 end
