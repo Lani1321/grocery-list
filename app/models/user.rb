4e49e5d12079880c56af63be47eff0dab4ac515a
Lani1321 <lani.berry13@gmail.com>
2016-10-03 23:06:20 -0400

    setup associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,13 @@
-user.rb
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :lists
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|user| user.slug == slug}
+  end
+  
+end
