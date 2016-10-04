691245b805abdc2a34e91f63e5c18e668e9e489b
Lani1321 <lani.berry13@gmail.com>
2016-10-03 23:23:43 -0400

    setup associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,15 @@
 class List < ActiveRecord::Base
   belongs_to :user
+  has_many :list_items
+  has_many :items, through: :list_items
+
+  
+  def slug
+    name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    List.all.find {|list| list.slug == slug}
+  end
+
 end
