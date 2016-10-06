cfa71ef0b26ed860eabe58080e929daf517f6361
Lani1321 <lani.berry13@gmail.com>
2016-10-05 20:12:59 -0400

    remove if statement from post '/signup' route for validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,10 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == "" 
-      redirect "/signup"
-    else
-      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
-      session[:user_id] = @user.id
-      redirect "/lists"
-    end
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+    session[:user_id] = @user.id
+    redirect "/"
   end
 
   get '/login' do
@@ -31,7 +27,7 @@ class UsersController < ApplicationController
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/lists"
+      redirect "/"
     else
       redirect to '/signup'
     end
