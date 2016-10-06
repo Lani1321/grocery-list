c1129d031783205e45214a0cfb60bfd01be69b54
Lani1321 <lani.berry13@gmail.com>
2016-10-06 18:58:11 -0400

    took out slug methods and changed validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :lists
   has_many :items, through: :lists
-  validates_uniqueness_of :username, :message => 'Oops, looks like someone already has this username, please try another'
+  validates :username, :uniqueness => {:message => "username already exists."}
   validates_presence_of :username, :email, :password
-  
-  def slug
-    self.username.downcase.gsub(" ", "-")
-  end
 
-  def self.find_by_slug(slug)
-    self.all.find{|user| user.slug == slug}
-  end
-  
 end
