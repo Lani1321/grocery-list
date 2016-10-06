675147b9f3551d85ed017f7f6b180806a44b28e3
Lani1321 <lani.berry13@gmail.com>
2016-10-05 23:58:27 -0400

    add get and post routes for create action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,5 +8,19 @@ class ListsController < ApplicationController
     erb :'lists/show'
   end
 
- 
+  # Create Action
+  get '/lists/new' do
+    redirect_if_not_logged_in
+    @user = current_user
+    @lists = List.all   #i don't think I need this line
+    erb :'/lists/create_list'
+  end
+
+
+  post '/lists/new' do
+    @list = List.create(:name => params[:name], :user_id => current_user.id)
+    @list.items.create(:name => params[:item])
+    redirect "/lists"
+    end
+
 end
