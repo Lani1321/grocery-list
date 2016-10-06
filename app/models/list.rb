7fb1bc4151b2cac9c4178a1a8e19230e30e32e46
Lani1321 <lani.berry13@gmail.com>
2016-10-06 18:57:17 -0400

    commented out slug methods

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,14 +1,13 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
-
   
-  def slug
-    name.downcase.gsub(" ", "-")
-  end
+  # def slug
+  #   name.downcase.gsub(" ", "-")
+  # end
 
-  def self.find_by_slug(slug)
-    List.all.find {|list| list.slug == slug}
-  end
+  # def self.find_by_slug(slug)
+  #   List.all.find {|list| list.slug == slug}
+  # end
 
 end
