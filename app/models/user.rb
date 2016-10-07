ecf3540da6d076e6fb8f04ed910b2086d9eb3108
Lani1321 <lani.berry13@gmail.com>
2016-10-07 09:53:42 -0400

    changed validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :lists
   has_many :items, through: :lists
-  validates :username, :uniqueness => {:message => "username already exists."}
+  validates_uniqueness_of :username
   validates_presence_of :username, :email, :password
 
 end
